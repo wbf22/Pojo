18b1fc984d8517b79812d6b8c6451796d028f8d0
wbf22 <brandon@fivefowlers.com>
2021-07-12 11:30:49 -0600

    updated readme again

---
+++ src/main/java/Student.java
@@ -0,0 +1,35 @@
+public class Student {
+
+  private java.lang.Integer grade;
+  private java.lang.Integer age;
+  private java.lang.Long ageInMilliseconds;
+
+
+  public Student() {}
+
+  public java.lang.Integer getGrade() {
+    return grade;
+  }
+
+  public void setGrade(java.lang.Integer grade) {
+    this. grade = grade;
+  }
+
+  public java.lang.Integer getAge() {
+    return age;
+  }
+
+  public void setAge(java.lang.Integer age) {
+    this. age = age;
+  }
+
+  public java.lang.Long getAgeInMilliseconds() {
+    return ageInMilliseconds;
+  }
+
+  public void setAgeInMilliseconds(java.lang.Long ageInMilliseconds) {
+    this. ageInMilliseconds = ageInMilliseconds;
+  }
+
+
+}
